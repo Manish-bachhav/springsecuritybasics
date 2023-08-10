83b5094357f438cce5eed682349506b0dc7c2145
Your Name <you@example.com>
2023-08-10 17:52:39 +0530

    basic security first commit

---
+++ src/test/java/com/practise/springsecuritybasics/SpringsecuritybasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
